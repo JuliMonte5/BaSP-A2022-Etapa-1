5805903ffd8d82e5eb2fbf179b5292be4932c7fc
JuliMonte5 <julian.montenegro.1895@gmail.com>
2022-09-15 18:27:09 -0300

    BaSP-W04-2022: added resolution for exercise 1

--- Semana-04/exercises/01_variables_and_operators.js
+++ Semana-04/exercises/01_variables_and_operators.js
@@ -1,1 +1,31 @@
-console.log('ola');
+console.log('1: Variables and Operators');
+
+/*1.a: create two numeric variables and utilize the addition operator to save the result of the sum in a 
+thrid variable.*/
+console.log('1.a');
+
+var x = 1, y = 2, z;
+
+z = x + y;
+
+console.log('Result: ',z);
+
+
+/*1.b: create to variables of type string and concatenate them saving the result in a third variable*/
+console.log('1.b');
+
+var string1 = 'Hello ', string2 = 'World', string3;
+
+string3 = string1 + string2;
+
+console.log('Result: ', string3);
+
+
+/*1.c: create two String type variables and sum the length of each variable, saving the result in a third variable*/
+console.log('1.c');
+
+var string1 = 'Item', string2 = 'c', stringSize;
+
+stringSize = string1.length + string2.length;
+
+console.log('Result :', stringSize);
