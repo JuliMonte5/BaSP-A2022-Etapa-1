15612cbb3fdf2af2821eeedfa5d5b1db9bcd3196
JuliMonte5 <julian.montenegro.1895@gmail.com>
2022-09-16 16:45:38 -0300

    BaSP-W04-2022: added resolution for exercise 2

---
+++ Semana-04/exercises/02_strings.js
@@ -0,0 +1,78 @@
+console.log('--2: Strings');
+
+/*Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en 
+mayúscula (utilizar toUpperCase).*/
+console.log('Exercise 2.a');
+
+var stringExerciseTwo = 'This is a string for exercise two a';
+
+stringExerciseTwo = stringExerciseTwo.toUpperCase();
+
+console.log('Answer 2.a: ', stringExerciseTwo);
+
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres
+guardando el resultado en una nueva variable (utilizar substring)..*/
+
+console.log('Exercise 2.b');
+
+var stringResultTwo;
+ 
+stringExerciseTwo = 'This is a string for exercise two b';
+
+stringResultTwo = stringExerciseTwo.substring(0, 10);
+
+console.log('Answer 2.b: ', stringResultTwo);
+
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres
+guardando el resultado en una nueva variable (utilizar substring).*/
+
+console.log('Exercise 2.c');
+
+stringExerciseTwo = 'This is a string for exercise two';
+
+stringResultTwo = stringExerciseTwo.substring((stringExerciseTwo.length - 3), stringExerciseTwo.length);
+
+console.log('Answer 2.c: ', stringResultTwo);
+
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en 
+mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, 
+toLowerCase y el operador +).*/
+
+console.log('Exercise 2.d');
+
+stringExerciseTwo = 'this is a string for exercise Two D';
+
+stringResultTwo = (stringExerciseTwo.substring(0,1)).toUpperCase() + stringExerciseTwo.substring(1,stringExerciseTwo.length).toLowerCase();
+
+console.log('Answer 2.d: ', stringResultTwo);
+
+/*Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición 
+del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+
+console.log('Exercise 2.e');
+
+var spaceIndex;
+
+stringExerciseTwo = 'this is a string for exercise two e';
+
+spaceIndex = stringExerciseTwo.indexOf(' ');
+
+console.log('Answer 2.e: ', spaceIndex);
+
+/*Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
+Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de 
+ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, 
+toLowerCase y el operador +).*/
+
+console.log('Exercise 2.f');
+
+var firstAuxliaryStringTwo, secondAuxliaryStringTwo;
+
+stringExerciseTwo = 'imagination deficiency';
+
+firstAuxliaryStringTwo = stringExerciseTwo.substring(0,1).toUpperCase() + (stringExerciseTwo.substring(1,stringExerciseTwo.indexOf(' '))).toLowerCase();
+secondAuxliaryStringTwo = stringExerciseTwo.substring(stringExerciseTwo.indexOf(' ') + 1,stringExerciseTwo.indexOf(' ') + 2).toUpperCase() + (stringExerciseTwo.substring(stringExerciseTwo.indexOf(' ') + 2,stringExerciseTwo.length)).toLowerCase();
+
+stringResultTwo = firstAuxliaryStringTwo + ' ' + secondAuxliaryStringTwo;
+
+console.log('Answer 2.f: ', stringResultTwo);
