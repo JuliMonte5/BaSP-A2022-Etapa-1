86bee555e663b9c5cd82d71906d09d6e26ec4d02
JuliMonte5 <julian.montenegro.1895@gmail.com>
2022-09-24 18:33:39 -0300

    BaSP-W05: finished logic for login page

--- Semana-05/javascript/login.js
+++ Semana-05/javascript/login.js
@@ -1,33 +1,45 @@
 window.onload = function() {
     var validateEmailInput, validatePasswordInput, emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;;
     var inputFields = document.getElementsByTagName("input");
-    console.log(inputFields[0]);
+    var contactFields = document.getElementsByClassName("cont-field");
+    var pErrors = document.getElementsByClassName("font-red");
+    var emailField = document.getElementsByName("Email");
+    var passwordField = document.getElementsByName("Password");
+    var passwordInput;
+    var emailInput;
 
     //Email validation
-    inputFields[0].onfocus = function() {
-        inputFields[0].removeAttribute("placeholder");
+    emailField[0].onfocus = function() {
+        emailField[0].removeAttribute("placeholder");
+        pErrors[0].classList.remove("display-flex");
+        pErrors[1].classList.remove("display-flex");
     }
-    inputFields[0].onblur = function() {
-        var emailInput;
-        emailInput = inputFields[0].value;
-        if (emailInput == emailExpression){
+    emailField[0].onblur = function() {
+        emailInput = emailField[0].value;
+        if (emailInput.match(emailExpression)){
             validateEmailInput = true;
         }
         else {
             validateEmailInput = false;
         }
+
         if(emailInput == ''){
-            inputFields[0].setAttribute('placeholder','Your Email Here');
+            pErrors[1].classList.add("display-flex");
+        }
+
+        if(!validateEmailInput && emailInput != ''){
+            pErrors[0].classList.add("display-flex");
         }
     }
 
     //Password validation
-    inputFields[1].onfocus = function() {
-        inputFields[1].removeAttribute("placeholder");
+    passwordField[0].onfocus = function() {
+        passwordField[0].removeAttribute("placeholder");
+        pErrors[2].classList.remove("display-flex");
+        pErrors[3].classList.remove("display-flex");
     }
-    inputFields[1].onblur = function() {
-        var passwordInput;
-        passwordInput = (inputFields[1].value).toLowerCase();
+    passwordField[0].onblur = function() {
+        passwordInput = (passwordField[0].value).toLowerCase();
         for(var i = 0; i < passwordInput.length;i++){
             if((passwordInput.charCodeAt(i) >= 97) && (passwordInput.charCodeAt(i) <= 122)){
                 validatePasswordInput = true;
@@ -39,25 +51,26 @@ window.onload = function() {
                 validatePasswordInput = false;
             }
         }
+
         if(passwordInput == ''){
-            inputFields[1].setAttribute('placeholder','Your Password Here');
+            pErrors[3].classList.add("display-flex");
+        }
+
+        if(!validatePasswordInput && passwordInput != ''){
+            pErrors[2].classList.add("display-flex");
         }
     }
 
     //Buttons Logic
     var loginButton = document.querySelector("button:nth-child(1)");
-    loginButton.onclick = function(){
+    loginButton.onclick = function(e){
+        e.preventDefault();
         if(validateEmailInput && validatePasswordInput){
             alert('Email: '+emailInput+'\nPassword: '+passwordInput+'');
         }
         else {
-            alert('Invalid inputs, please check')
+            alert('Invalid inputs, please check');
         }
-
-        /* if(!validateEmailInput){
-            
-        } */
     }
-    
     //Go Back Button
 }
